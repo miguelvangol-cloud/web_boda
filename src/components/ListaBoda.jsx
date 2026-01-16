9f63288030d9cd64841ff8f800b440ac73c6764a
Miguel Alonso <miguelvangol@gmail.com>
2026-01-16 21:31:50 +0100

    update web

--- src/components/ListaBoda.jsx
+++ src/components/ListaBoda.jsx
@@ -3,18 +3,22 @@ import React from 'react';
 export default function ListaBoda() {
   return (
     <section id="lista-boda" className="lista-section">
-      <div className="lista-grid">
+      <div className="lista-container">
+        <div className="lista-content">
+          <h3 className="lista-title">Lista de boda</h3>
+          <p className="lista-description">
+            Lo más importante ya lo tenemos: compartir este día con vosotros. Si además queréis ayudarnos en esta nueva etapa, estaremos muy agradecidos.
+          </p>
+          <div className="lista-iban-container">
+            <span className="lista-iban-label">Número de cuenta (IBAN)</span>
+            <p className="lista-iban">ES81 2100 8014 6002 0008 5555</p>
+            <p className="lista-iban-name">Celia Gómez Labrador</p>
+            <p className="lista-iban-name">Miguel Alonso Sánchez</p>
+          </div>
+          <img src="/images/photos/flor.png" alt="Flor" className="lista-ornament" />
+        </div>
         <div className="lista-image-col">
           <img src="/images/photos/pedida1.jpg" alt="Pareja" className="lista-img" />
-        </div>
-        <div className="lista-content">
-          <div className="lista-content-inner">
-            <h3 className="location-title">Lista de boda</h3>
-            <p className="lista-description">
-            Vuestra presencia es nuestro mayor regalo. No obstante, si queréis tener un detalle con nosotros, podéis hacerlo a través de la siguiente cuenta:
-          </p>
-            <p className="lista-iban">ES12 3456 7890 1234 5678</p>
-          </div>
         </div>
       </div>
     </section>
--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -16,8 +16,9 @@ export default function Location() {
 
           <aside className="location-aside">
             <div className="location-callout">
-              <a href="https://www.google.com/maps/search/?api=1&query=Cigarral+del+Ángel+Toledo" target="_blank" rel="noopener noreferrer" aria-label="Ver ubicación en Google Maps">
-                <img src="/images/photos/ubicacion.png" alt="Vista del Cigarral" />
+              <img src="/images/photos/ubicacion.png" alt="Vista del Cigarral" className="location-image" />
+              <a href="https://www.google.com/maps/search/?api=1&query=Cigarral+del+Ángel+Toledo" target="_blank" rel="noopener noreferrer" aria-label="Ver ubicación en Google Maps" className="location-map-link">
+                <img src="/images/photos/como_llegar.png" alt="Cómo llegar" className="location-map-image" />
               </a>
             </div>
           </aside>
